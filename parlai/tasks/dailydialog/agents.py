2d40b5995ffc50570d1c3c98cb776f768aa737ff
Vitou <phy_vitou@live.comE>
2020-01-22 17:16:35 +0900

    add rerank dataset to dailydialog

--- parlai/tasks/dailydialog/agents.py
+++ parlai/tasks/dailydialog/agents.py
@@ -21,7 +21,8 @@ once acting as Speaker 2.
 
 import os
 import json
-from parlai.core.teachers import FixedDialogTeacher
+import random
+from parlai.core.teachers import FixedDialogTeacher, FbDialogTeacher
 from parlai.core.params import ParlaiParser
 from .build import build
 
@@ -188,6 +189,115 @@ class ContextTeacher(Convai2Teacher):
         }
 
         return action
+
+class RerankTeacher(Convai2Teacher):
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt, shared)
+
+        # Calculate the correct number of examples.
+        self.num_exs = sum(len(d['dialogue']) - 1 for d in self.data)
+
+        # Store all episodes separately, so we can deal with 2-turn dialogs.
+        self.all_eps = self.data + [d for d in self.data if len(d['dialogue']) > 2]
+        self.num_eps = len(self.all_eps)
+
+        self.history_size = 5
+        self.start_token =  "<SOU>"
+        self.speaker_a = "<speaker_A>"
+        self.speaker_b = "<speaker_B>"
+        self.end_token = "<EOU>"
+
+        self.candidates = self.load_candidates(self.get_data_folder())
+
+    def get_data_folder(self):
+        dir_path = os.path.join(self.opt['datapath'], self.opt['task'].split(':')[0])
+        cand_path = os.path.join(dir_path, "cands_{}.txt".format(self.opt['datatype']))
+        return cand_path
+
+    def load_candidates(self, cand_path):
+        with open(cand_path) as file:
+            candidates = file.readlines()
+            return candidates
+
+    def get_candidates(self, groundtruth, num_cands=10, include_gt=True):
+
+        # if we select all, just remove ONE grouthtruth from there
+        if num_cands < 0:
+            candidates = self.candidates.copy()
+            match_indices = []
+            for idx, cand in enumerate(candidates):
+                if cand.strip() == groundtruth.strip():
+                    match_indices.append(idx)
+            candidates.pop(match_indices[0])
+            return candidates
+            
+            
+        # case that num_cands > 0
+        total_cands = len(self.candidates)
+        candidates = []
+
+        # get 10 candidates. One groundtruth + 9 fake
+        while len(candidates) < num_cands-1:
+            idx = random.randint(0, total_cands-1)
+            if self.candidates[idx] == groundtruth:
+                continue
+            candidates.append(self.candidates[idx])
+
+        candidates.append(groundtruth)
+        return candidates
+        
+
+    def get(self, episode_idx, entry_idx=0):
+        full_eps = self.all_eps[episode_idx]
+        entries = full_eps['dialogue']
+        # Sometimes we're speaker 1 and sometimes we're speaker 2.
+        # We can't be speaker 1 if dialog has only 2 turns.
+        
+        speaker_id = int(episode_idx >= len(self.data))
+        end_turn_idx = speaker_id + 2 * entry_idx
+        start_turn_idx = max(0, end_turn_idx - self.history_size + 1)
+        their_turns = entries[start_turn_idx : end_turn_idx+1]
+        my_turn = entries[1 + speaker_id + 2 * entry_idx]
+        episode_done = 2 * entry_idx + speaker_id + 1 >= len(entries) - 2
+
+        context_emotions = []
+        context_acts = []
+        context_texts = []
+
+        if self.opt['datatype'] in ['train', 'valid']:
+            candidates = self.get_candidates(my_turn['text'], num_cands=10, include_gt=True)
+        else:
+            candidates = self.get_candidates(my_turn['text'], num_cands=-1, include_gt=False)
+
+        for idx, turn in enumerate(their_turns):
+            context_emotions.append(turn['emotion'])
+            context_acts.append(turn['act'])
+
+            context_texts.append(self.start_token)
+            speaker_label = self.speaker_b if idx % 2 == 0 else self.speaker_a
+            context_texts.append(speaker_label)
+            context_texts.append(turn['text'])
+            context_texts.append(self.end_token)
+
+        context_text = " ".join(context_texts)
+
+        action = {
+            'topic': full_eps['topic'],
+            'text': context_text,
+            'emotion': context_emotions,
+            'act_type': context_acts,
+            'labels': [my_turn['text']],
+            'label_candidates': candidates,
+            'labels_emotion': my_turn['emotion'],
+            'labels_act': my_turn['act'],
+            'episode_done': episode_done,
+        }
+
+        return action
+    
+
+    
 
 class DefaultTeacher(Convai2Teacher):
     pass
