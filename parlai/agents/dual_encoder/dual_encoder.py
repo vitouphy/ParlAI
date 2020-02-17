98c8d827423bc180e0a3a44297dc05e71556bd37
Vitou <phy_vitou@live.comE>
2020-02-17 15:05:42 +0700

    clean up code for dual encoder

--- parlai/agents/dual_encoder/dual_encoder.py
+++ parlai/agents/dual_encoder/dual_encoder.py
@@ -12,53 +12,6 @@ from parlai.core.torch_ranker_agent import TorchRankerAgent
 import torch
 from torch import nn
 
-
-class ExampleBagOfWordsModel(nn.Module):
-    """
-    This constructs a simple bag of words model.
-    It contains a encoder for encoding candidates and context.
-    """
-
-    def __init__(self, opt, dictionary):
-        super().__init__()
-        self.hidden_dim = opt.get('hidden_dim', 512)
-        self.dict = dictionary
-        self.encoder = nn.EmbeddingBag(len(self.dict), self.hidden_dim)
-
-    def encode_text(self, text_vecs):
-        """
-        This function encodes a text_vec to a text encoding.
-        """
-        return self.encoder(text_vecs)
-
-    def forward(self, batch, cand_vecs, cand_encs=None):
-        bsz = cand_vecs.size(0)
-        if cand_encs is None:
-            if cand_vecs.dim() == 3:
-                # if dim = 3, bsz *  num_candidates * seq_length
-                # In this case, we are using inline candidates
-                cand_vecs = cand_vecs.reshape(-1, cand_vecs.size(2))
-            cand_encs = self.encode_text(cand_vecs)
-            if cand_encs.size(0) != bsz:
-                # Some cases, we could also use batch candidates,
-                # they will be size of bsz * seq_length
-                # so we don't have to use the extra treatment
-                cand_encs = cand_encs.reshape(bsz, -1, self.hidden_dim)
-        context_encodings = self.encode_text(batch.text_vec)
-        print ("context size: ", )
-        print (context_encodings.size())
-        print ("cand size: ", )
-        print (cand_encs.size())
-        print ("ctx unsqueenze: ", )
-        print (context_encodings.unsqueeze(1).size())
-        
-        if context_encodings.dim() != cand_encs.dim():
-            return torch.sum(
-                context_encodings.unsqueeze(1).expand_as(cand_encs) * cand_encs, 2
-            )
-        print ("HIII")
-        return context_encodings.mm(cand_encs.t())
-
 class DualEncoder(nn.Module):
     """
     This constructs a simple bag of words model.
@@ -117,7 +70,7 @@ class DualEncoder(nn.Module):
 
         return score
 
-class TraAgent(TorchRankerAgent):
+class DualEncoderAgent(TorchRankerAgent):
     """
     Example subclass of TorchRankerAgent.
     This particular implementation is a simple bag-of-words model, which demonstrates
@@ -141,20 +94,7 @@ class TraAgent(TorchRankerAgent):
         None (when we cache the encoding of the candidate vectors), you may use these
         instead of calling self.model on `cand_vecs`.
         """
-
-        # print (batch.keys())
-        # print (batch['observations'][0].keys())
-        # print (batch['observations'][0]['text_vec'])
-        # print (batch['observations'][0]['text'])
-        # print ("-------------------------")
-        # # print (batch['label_vec'][0])
-        # # print (batch['candidate_vecs'][0])
-        # # print (batch['valid_indices'][0])
-        # print (cand_vecs[0])
-        # print ("=========================\n")
         scores = self.model.forward(batch, cand_vecs, cand_encs)
-        # print (scores.size())
-        # return
         return scores
 
     def build_model(self):
